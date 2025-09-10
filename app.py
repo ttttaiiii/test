f4861cf8a45c00ee1a775dfc1c41da4933e8e480
ttttaiiii <taitumeng@gmail.com>
2025-09-10 08:57:48 -0400

    add port

--- app.py
+++ app.py
@@ -20,5 +20,8 @@ app.layout = html.Div([
     page_container
 ])
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
